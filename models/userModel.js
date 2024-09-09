2a69a9506b66bdcf80b05cd5e1694f89257cf0bc
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-09-09 22:16:33 +0500

    user

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+        }
+})
+
+// Static signup method
+userSchema.statics.signup = async function(email, password) {
+    const userExist = await this.findOne({ email });
+    if (userExist) {
+        throw Error('Email already in use');
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    const user = await this.create({ email, password: hashedPassword })
+
+    return user
+}
+
+module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require('cors');
 const mongoose = require('mongoose')
 const workoutRoutes = require("./routes/workouts")
+const userRoutes = require("./routes/user")
 const app = express()
 
 // Enable CORS for all routes
@@ -16,6 +17,7 @@ app.use((req, res, next) =>{
 })
 // Request Handeling
 app.use('/api/workout', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
