c47e7b8d66b70a437d65316c0f45ac2c575adb3f
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-09-10 16:28:25 +0500

    login done

--- models/userModel.js
+++ models/userModel.js
@@ -44,4 +44,22 @@ userSchema.statics.signup = async function(email, password) {
     return user
 }
 
+userSchema.statics.login = async function(email, password){
+    if(!email || !password) {
+        throw Error("All fields are required")
+    }
+
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw Error('Incorrect Email');
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect Password')
+    }
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
