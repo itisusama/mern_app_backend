f65bf0b7142e5fcd54bc59dd01edab0f1f44cf7a
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-09-10 14:55:53 +0500

    jwt token

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require("validator")
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,19 @@ const userSchema = new Schema({
 
 // Static signup method
 userSchema.statics.signup = async function(email, password) {
+    
+    if(!email || !password) {
+        throw Error("All fields are required")
+    }
+    
+    if(!validator.isEmail(email)){
+        throw Error("Invalid email")
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is weak")
+    }
+
     const userExist = await this.findOne({ email });
     if (userExist) {
         throw Error('Email already in use');
